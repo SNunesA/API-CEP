4d641edb7e46c7221a53be865050a697a9b255c5
SNunesA <snunesadiers@gmail.com>
2023-03-07 20:17:27 -0300

    terminado

--- consulta_cep.js
+++ consulta_cep.js
@@ -1,9 +1,14 @@
 
 function preencherFormulario(endereco){
+    console.log(endereco)
     document.getElementById("endereco").value = endereco.logradouro;
     document.getElementById("bairro").value = endereco.bairro;
     document.getElementById("cidade").value = endereco.localidade;
     document.getElementById("estado").value = endereco.uf;
+
+    document.getElementById("IBGE").value = endereco.ibge;
+    document.getElementById("DDD").value = endereco.ddd;
+    document.getElementById("Siafi").value = endereco.siafi;
 }
 
 function eNumero(numero){
@@ -37,6 +42,9 @@ async function pesquisarCEP(){
         document.getElementById("bairro").value= ""
         document.getElementById("cidade").value= ""
         document.getElementById("estado").value= ""
+        document.getElementById("IBGE").value= ""
+        document.getElementById("DDD").value= ""
+        document.getElementById("Siafi").value= "" 
         
     }
 }
@@ -50,8 +58,62 @@ function limpacampo(){
     document.getElementById("bairro").value= ""
     document.getElementById("cidade").value= ""
     document.getElementById("estado").value= ""  
+    document.getElementById("IBGE").value= ""
+    document.getElementById("DDD").value= ""
+    document.getElementById("Siafi").value= "" 
+}
+
+function salvar(){
+    var texto="";
+    var variavel = document.getElementById("nome").value;
+    if ( variavel != ""){
+        texto="Nome:"+variavel+" \n";
+    }
+    var variavel = document.getElementById("email").value;
+    if ( variavel != ""){
+        texto=texto+"Email:"+variavel+" \n";
+    }
+    var variavel = document.getElementById("cep").value;
+    if ( variavel != ""){
+        texto=texto+"CEP:"+variavel+" \n";
+    }
+    var variavel = document.getElementById("endereco").value;
+    if ( variavel != ""){
+        texto=texto+"Endereço:"+variavel+" \n";
+    }
+    var variavel = document.getElementById("numero").value;
+    if ( variavel != ""){
+        texto=texto+"N°:"+variavel+" \n";
+    }
+    var variavel = document.getElementById("bairro").value;
+    if ( variavel != ""){
+        texto=texto+"Bairro:"+variavel+" \n";
+    }
+    var variavel = document.getElementById("cidade").value;
+    if ( variavel != ""){
+        texto=texto+"Cidade:"+variavel+" \n";
+    }
+    var variavel = document.getElementById("estado").value;
+    if ( variavel != ""){
+        texto=texto+"Estado:"+variavel+" \n";
+    }
+    var variavel = document.getElementById("IBGE").value;
+    if ( variavel != ""){
+        texto=texto+"IBGE:"+variavel+" \n";
+    }
+    var variavel = document.getElementById("DDD").value;
+    if ( variavel != ""){
+        texto=texto+"DDD:"+variavel+" \n";
+    }
+    var variavel = document.getElementById("Siafi").value;
+    if ( variavel != ""){
+        texto=texto+"Siafi:"+variavel+" \n";
+    }
+    alert(texto);
 }
 
 //focusout é quando o usuario terminou de inserir e tira o mouse daquele campo
 document.getElementById("cep").addEventListener("focusout",pesquisarCEP);
 
+document.getElementById("btLimpar").addEventListener("click",limpacampo);
+document.getElementById("btSalvar").addEventListener("click",salvar);
